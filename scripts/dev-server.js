3a8285da6843a4323766ae4933ae46649bc0ca64
Fares Schulz <schulzfares@gmail.com>
2025-07-30 12:15:48 +0200

    One more change of folder structure

--- scripts/dev-server.js
+++ scripts/dev-server.js
@@ -80,7 +80,7 @@ async function startDevServer() {
 async function watchFiles() {
   const watchDirs = [
     path.join(__dirname, '../slides/src/css'),
-    path.join(__dirname, '../slides/src/content'),
+    path.join(__dirname, '../slides/content'),
     path.join(__dirname, '../slides/src/js'),
     path.join(__dirname, '../slides/templates'),
     path.join(__dirname, '../slides/assets')
---
+++ slides/content/01-intro.md
@@ -0,0 +1,10 @@
+# Deep Learning for Audio Data
+
+Note:
+Welcome everyone to this presentation about Reveal.js. This is the opening slide where I'll introduce the topic. Remember to speak slowly and make eye contact with the audience.
+
+---
+
+## Lecturer
+
+Fares Schulz
